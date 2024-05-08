250f48315bfe75250c7370c19eaefa67fee89742
Julian Bilcke <julian.bilcke@huggingface.co>
2024-05-08 13:07:55 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,12 @@ export default function Page() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86" />
-        <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      </Head>
+      <Script id="gtm">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-K98T8ZFZ');`}</Script>
-      </Head>
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K98T8ZFZ"
 height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
       <main className={cn(
